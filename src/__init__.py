fcffad1ce72c54f2cf3069ea6173172826b09f54
Dale Percelay <dalepercelay@Dales-Air.attlocal.net>
2025-03-02 10:33:21 -0800

    test

---
+++ src/__init__.py
@@ -0,0 +1,4 @@
+import pandas as pd
+from glob import glob
+
+pd.read_csv("../src/data/raw/MetaMotion/A-bench-heavy2-rpe8_MetaWear_2019-01-11T16.10.08.270_C42732BE255C_Accelerometer_12.500Hz_1.4.4.csv")
